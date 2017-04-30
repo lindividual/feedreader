c42360d7a2baf8c07cf164d7cd3dff276157493d
Deng Haolin <1020958553@qq.com>
2017-04-30 18:58:44 +0800

    fix typo erro

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,24 +31,22 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('each feed has a url defined and the url is not empty'), function() {
+         it('each feed has a url defined and the url is not empty', function() {
            allFeeds.forEach(function(feed){
              expect(feed.url).toBeDefined();
              expect(feed.url.length).not.toBe(0);
-           });
-         };
+          });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('each feed has name defined and the name is not empty'), function() {
+         it('each feed has name defined and the name is not empty', function() {
            allFeeds.forEach(function(feed){
              expect(feed.name).toBeDefined();
              expect(feed.name.length).not.toBe(0);
-           });
-         };
+          });
     });
 
 
